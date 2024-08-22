089d0dd5ee9eec330b6c34519d374c4344775525
Mabas <mabavepe2410@hotmail.com>
2024-08-22 11:05:37 -0600

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by mabas on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
